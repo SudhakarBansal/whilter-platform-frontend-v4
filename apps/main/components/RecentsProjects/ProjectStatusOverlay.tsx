ce8f0996c84ad385d7dc8c8cc95420ad4af4b5b8
Sudhakar bansal <sidhu1171bansal@gmail.com>
2025-06-13 16:25:01 +0530

    minor fix

--- apps/main/components/RecentsProjects/ProjectStatusOverlay.tsx
+++ apps/main/components/RecentsProjects/ProjectStatusOverlay.tsx
@@ -4,7 +4,7 @@ import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
 export const ProjectStatusOverlay: React.FC<ProjectStatusOverlayProps> = ({ status, progress }) => {
   return (
-    <Box className="absolute inset-0 flex items-center justify-center flex-col z-10 bg-black/75 rounded">
+    <Box className="absolute inset-0 flex items-center justify-center flex-col z-10 bg-black/75">
       {status === 'in-process' && progress !== undefined && (
         <Box className="w-4/5 text-center">
           <Typography variant="subtitle1" className="mb-2">
