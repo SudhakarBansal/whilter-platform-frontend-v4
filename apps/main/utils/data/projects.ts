1374eeecb2101a52b2b79b9f56d26166073c8769
Sudhakar bansal <sidhu1171bansal@gmail.com>
2025-06-16 12:05:47 +0530

    make folder card

---
+++ apps/main/utils/data/projects.ts
@@ -0,0 +1,33 @@
+export const projectData = {
+    "folders": [
+        {
+            "title": "Design System",
+            "projectCount": 15,
+            "avatars": [
+                { "initial": "A", "bgColor": "bg-red-500" },
+                { "initial": "B", "bgColor": "bg-amber-500" },
+                { "initial": "C", "bgColor": "bg-purple-500" },
+                { "initial": "+2", "bgColor": "bg-gray-400" }
+            ]
+        },
+        {
+            "title": "Marketing Assets",
+            "projectCount": 8,
+            "avatars": [
+                { "initial": "J", "bgColor": "bg-amber-500" },
+                { "initial": "L", "bgColor": "bg-red-500" },
+                { "initial": "+3", "bgColor": "bg-gray-400" }
+            ]
+        },
+        {
+            "title": "Tools Projects",
+            "projectCount": 10,
+            "avatars": [
+                { "initial": "J", "bgColor": "bg-amber-500" },
+                { "initial": "M", "bgColor": "bg-red-500" },
+                { "initial": "S", "bgColor": "bg-purple-500" },
+                { "initial": "+1", "bgColor": "bg-gray-400" }
+            ]
+        }
+    ]
+};
