fb8df0c09f6272d4591f980b6752877947117b5e
Sudhakar bansal <sidhu1171bansal@gmail.com>
2025-06-19 16:01:55 +0530

    add media tools sidebar data

---
+++ apps/media-tools/utils/menuItems.data.ts
@@ -0,0 +1,42 @@
+export const menuItems = [
+    {
+        icon: '/icons/Home.svg',
+        label: 'Home',
+        href: '/'
+    },
+    {
+        icon: '/icons/lip-sync.svg',
+        label: 'Lip Sync',
+        href: '/not-found'
+    },
+    {
+        icon: '/icons/video-replace.svg',
+        label: 'Video Replace',
+        href: '/not-found'
+    },
+    {
+        icon: '/icons/voice-cloning.svg',
+        label: 'Voice Cloning',
+        href: '/not-found'
+    },
+    {
+        icon: '/icons/text-to-speech.svg',
+        label: 'Text to Speech',
+        href: '/not-found'
+    },
+    {
+        icon: '/icons/super-resolution.svg',
+        label: 'Super Resolution',
+        href: '/not-found'
+    },
+    {
+        icon: '/icons/image-scaler.svg',
+        label: 'Image Scaler',
+        href: '/not-found'
+    },
+    {
+        icon: '/icons/avatar-video.svg',
+        label: 'Avatar Video',
+        href: '/not-found'
+    }
+];
