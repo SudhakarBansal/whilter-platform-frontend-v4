068022b79fae566c0a2989a2f3837870092641d9
Sudhakar bansal <sidhu1171bansal@gmail.com>
2025-06-20 16:49:22 +0530

    minor fixes

--- packages/shared-layouts/index.ts
+++ packages/shared-layouts/index.ts
@@ -1,2 +1,2 @@
 export {PageLayout,pageLayoutPresets} from "./src/styled/page-layout.js";
-export {Layout as MainLayout} from "./src/main-layout/index.js";
+export {MainLayout} from "./src/main-layout/index.js";
