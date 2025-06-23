61a2621992ae6fe6b753a5657e0fd492e6f19e9a
Sudhakar bansal <sidhu1171bansal@gmail.com>
2025-06-23 11:53:07 +0530

    minor fix

--- apps/main/app/(platform)/page.tsx
+++ apps/main/app/(platform)/page.tsx
@@ -1,7 +1,7 @@
-'use client';
 import React from 'react';
 import { Button } from '@mui/material';
-import { PageLayout, pageLayoutPresets } from '@whilter/shared-layouts/styled';
+import { PageLayout } from '@whilter/shared-layouts/styled';
+import { pageLayoutPresets } from '@whilter/shared-layouts/styled';
 import { RecentProjects } from '@whilter/ui-kit/components';
 import { ServiceCards } from '@/components/ServiceCards';
 import { projectData } from '@/utils/data/projects';
