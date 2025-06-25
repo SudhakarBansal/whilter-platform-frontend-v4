28a8c3ec5d038dd9f27199d2db97fe1ecf409287
Sudhakar bansal <sidhu1171bansal@gmail.com>
2025-06-25 14:54:41 +0530

    fix use client

--- apps/media-tools/components/FeaturedTools/ToolsSection.tsx
+++ apps/media-tools/components/FeaturedTools/ToolsSection.tsx
@@ -3,7 +3,7 @@ import { toolsCardsData } from '@/data/toolsCard.data'
 import { ToolCard } from './ToolsCard';
 
 
-export const ToolCardsGrid: React.FC = () => {
+export const ToolCardsGrid = () => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
             {toolsCardsData.map((cardData) => (
