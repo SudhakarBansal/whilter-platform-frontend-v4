773fc6a008e8f7abb7dab037a078a0ad64e5ab60
Isha <ishasaini2275@gmail.com>
2025-07-09 14:50:46 +0530

    add campaign component

--- apps/charp-ai/app/campaign/page.tsx
+++ apps/charp-ai/app/campaign/page.tsx
@@ -1,8 +1,24 @@
+"use client";
 import DashboardLayout from "@/layouts/dashboard-layout";
 import { pageLayoutPresets } from "@whilter/shared-layouts/styled";
 import { buildBreadcrumbs } from "@/utils/buildBreadcrumbs";
+import { Button } from "@mui/material";
+import { useRouter } from "next/navigation";
+import CampaignCardSection from "@/components/campaign-section/CampaignCardSection";
 
 export default function ViewCampaignPage() {
+  const router = useRouter();
+
+  const actionButtons = [
+    <Button
+      key="view-campaign"
+      variant="flatPrimary"
+      className="text-lg p-4"
+      onClick={() => router.push("/campaign/new-brand")}
+    >
+      Add New Brand
+    </Button>,
+  ];
 
   const breadcrumbs = buildBreadcrumbs([
     { label: "Dashboard", href: "/" },
@@ -12,11 +28,12 @@ export default function ViewCampaignPage() {
   return (
     <DashboardLayout
       breadcrumbs={breadcrumbs}
-      heading="Campaigns of Dominos Pizza"
-      description="Choose campaign to view details"
+      heading="Brand List"
+      description="Choose Brand to manage campaigns"
       config={pageLayoutPresets.dashboard}
-
+      buttons={actionButtons}
     >
+      <CampaignCardSection />
 
     </DashboardLayout>
   );
---
+++ apps/charp-ai/components/campaign-section/CampaignCardSection.tsx
@@ -0,0 +1,33 @@
+import CampaignCard from "./CampaignCard"
+
+const campaigns = [
+    {
+        companyName: "HDFC BANK",
+        logoUrl: "/placeholder.svg?height=32&width=32",
+        totalCampaigns: 8,
+        liveCampaigns: 1,
+        activeCampaigns: 3,
+        isSelected: false,
+    },
+    {
+        companyName: "Domino's Pizza",
+        logoUrl: "/placeholder.svg?height=32&width=32",
+        totalCampaigns: 5,
+        liveCampaigns: 1,
+        activeCampaigns: 1,
+        isSelected: true,
+    },
+    
+]
+
+export default function CampaignCardSection() {
+
+    return (
+
+        <div className="w-full grid justify-center gap-12 [grid-template-columns:repeat(auto-fill,minmax(260px,max-content))]">
+            {campaigns.map((campaign, idx) => (
+                <CampaignCard key={idx} {...campaign} />
+            ))}
+        </div>
+    )
+}
