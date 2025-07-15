be4faa410e0410fad9b4ea7c7805f5da7196b94d
Sudhakar bansal <sidhu1171bansal@gmail.com>
2025-07-15 14:28:45 +0530

    minor fix

--- apps/media-tools/components/Fileicons.tsx
+++ apps/media-tools/components/Fileicons.tsx
@@ -6,8 +6,8 @@ interface FileIconProps {
 }
 
 export const FileIcon: React.FC<FileIconProps> = ({ fileType }) => {
-    if (fileType.startsWith('audio/')) return <AudioFile className="text-blue-500" />;
-    if (fileType.startsWith('video/')) return <VideoFile className="text-red-500" />;
-    if (fileType.startsWith('image/')) return <ImageIcon className="text-green-500" />;
+    if (fileType.startsWith('audio')) return <AudioFile className="text-blue-500" />;
+    if (fileType.startsWith('video')) return <VideoFile className="text-red-500" />;
+    if (fileType.startsWith('image')) return <ImageIcon className="text-green-500" />;
     return <Description className="text-gray-500" />;
 };
