989d965e67e781758d7433aff3e4e9969a4da98e
Sudhakar bansal <sidhu1171bansal@gmail.com>
2025-07-15 16:55:56 +0530

    add one more upload wrapper

--- apps/media-tools/components/tool-media-panels/VoiceCloneMediaPanel.tsx
+++ apps/media-tools/components/tool-media-panels/VoiceCloneMediaPanel.tsx
@@ -28,6 +28,20 @@ export function VoiceCloneMediaPanel() {
         <>
             <FileUploadWrapper
                 type="audio"
+                label="Source Audio"
+                heading="Upload Source Audio"
+                subheading="Add your Files here"
+                footer="Only support .wav, mp3 and Audio files"
+                acceptedFormats={['.wav', '.mp3', '.m4a']}
+                maxFileSize={10}
+                onUpload={handleUpload}               // Called after successful upload
+                onFileSelected={handleFileSelected}   // Called when file is selected
+                onFileRemoved={handleFileRemoved}     // Called when uploaded file is removed
+            />
+
+            <FileUploadWrapper
+                type="audio"
+                label="Recorded Audio"
                 heading="Upload Source Audio"
                 subheading="Add your Files here"
                 footer="Only support .wav, mp3 and Audio files"
