3f987d4d8207f9200457797abc8bac80c951b4a1
Sudhakar bansal <sidhu1171bansal@gmail.com>
2025-07-16 09:49:45 +0530

    add Video uploader in lip sync for testing

--- apps/media-tools/components/tool-media-panels/LipSyncMediaPanel.tsx
+++ apps/media-tools/components/tool-media-panels/LipSyncMediaPanel.tsx
@@ -1,7 +1,20 @@
+'use client';
+
+import FileUploadWrapper from "./FileUploadWrapper";
+
 export function LipSyncMediaPanel() {
     return (
-        <>
-            <p>Lip Sync Media Panel</p>
-        </>
+        <FileUploadWrapper
+            type="video"
+            label="Source Audio"
+            heading="Upload Source Audio"
+            subheading="Add your Files here"
+            footer="Only support .wav, mp3 and Audio files"
+            acceptedFormats={['.mp4']}
+            maxFileSize={10}
+            // onUpload={handleUpload}               // Called after successful upload
+            // onFileSelected={handleFileSelected}   // Called when file is selected
+            // onFileRemoved={handleFileRemoved}     // Called when uploaded file is removed
+        />
     );
 }
