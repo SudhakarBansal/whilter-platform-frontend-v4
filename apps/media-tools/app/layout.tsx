9f3ebf93466fb83cd5206968770ef3e382d85d15
Sudhakar bansal <sidhu1171bansal@gmail.com>
2025-07-22 14:28:18 +0530

    add toast in layout

--- apps/media-tools/app/layout.tsx
+++ apps/media-tools/app/layout.tsx
@@ -2,7 +2,8 @@ import "@whilter/ui-kit/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeConfig } from "@whilter/config";
-import { MainLayout} from "@whilter/shared-layouts/main"
+import { MainLayout } from "@whilter/shared-layouts/main"
+import { Toaster } from 'sonner';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Toaster
+          position="top-right"
+          expand={true}
+          richColors={true}
+          closeButton={true}
+          toastOptions={{
+            duration: 4000,
+          }} />
         <ThemeConfig>
           <MainLayout>
             {children}
