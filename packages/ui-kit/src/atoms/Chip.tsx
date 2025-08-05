7e17c685325686b6e3ca759993a08ff7b82c1499
Sudhakar bansal <sidhu1171bansal@gmail.com>
2025-08-05 12:38:41 +0530

    add padding in chip

--- packages/ui-kit/src/atoms/Chip.tsx
+++ packages/ui-kit/src/atoms/Chip.tsx
@@ -1,4 +1,4 @@
-import type { Theme } from '@mui/material/styles';
+import type { Theme } from "@mui/material/styles";
 
 // ==============================|| OVERRIDES - CHIP ||============================== //
 
@@ -8,35 +8,36 @@ export default function Chip(theme: Theme) {
       styleOverrides: {
         root: {
           borderRadius: theme.spacing(8),
-          '&:active': {
-            boxShadow: 'none',
+          padding: theme.spacing(1.5),
+          "&:active": {
+            boxShadow: "none",
           },
-          '&.MuiChip-deletable .MuiChip-deleteIcon': {
-            color: 'inherit',
+          "&.MuiChip-deletable .MuiChip-deleteIcon": {
+            color: "inherit",
           },
-          '& .MuiSvgIcon-fontSizeSmall': {
-            fontSize: '1rem',
+          "& .MuiSvgIcon-fontSizeSmall": {
+            fontSize: "1rem",
           },
         },
         sizeLarge: {
-          fontSize: '1rem',
+          fontSize: "1rem",
           height: 40,
         },
         light: {
           color: theme.palette.primary.main,
           backgroundColor: theme.palette.primary.light,
           borderColor: theme.palette.primary.light,
-          '&.MuiChip-lightError': {
+          "&.MuiChip-lightError": {
             color: theme.palette.error.main,
             backgroundColor: theme.palette.error.light,
             borderColor: theme.palette.error.light,
           },
-          '&.MuiChip-lightSuccess': {
+          "&.MuiChip-lightSuccess": {
             color: theme.palette.success.main,
             backgroundColor: theme.palette.success.light,
             borderColor: theme.palette.success.light,
           },
-          '&.MuiChip-lightWarning': {
+          "&.MuiChip-lightWarning": {
             color: theme.palette.warning.main,
             backgroundColor: theme.palette.warning.light,
             borderColor: theme.palette.warning.light,
