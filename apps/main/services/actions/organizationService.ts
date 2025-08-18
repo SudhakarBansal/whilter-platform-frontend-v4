a5e595d36951d1cbbb068cd5f4872ae0db36e3d4
Sudhakar bansal <sidhu1171bansal@gmail.com>
2025-08-18 17:00:05 +0530

    remove mock timeout in organizarion listing

--- apps/main/services/actions/organizationService.ts
+++ apps/main/services/actions/organizationService.ts
@@ -143,7 +143,6 @@ export async function getPaginatedOrganizationWithFilters(
   }): Promise<PaginatedFilterOrganizationResponse> {
   console.log("params --organization", params);
   try {
-    await new Promise(resolve => setTimeout(resolve, 2000));
     const response = await axiosInstance.get(ServiceEndpoints.organization.paginatedOrganizationWithFilter, { params: params });
     return response.data;
   } catch (error: any) {
