9f599392d3070e4d623f8f7c3187f164a0f17c94
Isha <ishasaini2275@gmail.com>
2025-08-19 11:38:36 +0530

    issue fixing in user-management

--- apps/main/app/(root)/(settings)/user-management/components/users-list.tsx
+++ apps/main/app/(root)/(settings)/user-management/components/users-list.tsx
@@ -1,37 +1,50 @@
-import React from 'react'
-import { UserFilters } from './filter';
-import { UserCard } from './user-card';
-import { type User } from '@/services/service-types';
-import { type OptionType } from '../page';
+import React from "react";
+import { UserFilters } from "./filter";
+import { UserCard } from "./user-card";
+import { type User } from "@/services/service-types";
+import { type OptionType } from "../page";
+
 
 type UsersListProps = {
-    users: User[];
-    organizationList: OptionType[];
-    rolesList: OptionType[];
-    defaultValue?: string;
+  users: User[];
+  organizationList: OptionType[];
+  rolesList: OptionType[];
+  totalPages: number;
+  totalItems: number;
+  itemsPerPage: number;
+  children?: React.ReactNode
 };
 
-const UsersList = ({ users, organizationList, rolesList }: UsersListProps) => {
+const UsersList = ({
+  users,
+  organizationList,
+  rolesList,
+  totalPages,
+  totalItems,
+  itemsPerPage,
+  children
+}: UsersListProps) => {
+  return (
+    <div className="flex flex-col gap-6">
+      <UserFilters organizationList={organizationList} rolesList={rolesList} 
+      totalPages={totalPages}
+      totalItems={totalItems}
+      itemsPerPage={itemsPerPage}
+      />
 
-    return (
-        <div>
-            <UserFilters
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-10 gap-x-[55px] w-full">
+            {users.map((user, index) => (
+              <UserCard
+                key={index}
+                user={user}
                 organizationList={organizationList}
                 rolesList={rolesList}
-            />
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-10 gap-x-[55px] w-full">
-                {users.map((user, index) => (
-                    <UserCard
-                        key={index}
-                        user={user}
-                        organizationList={organizationList}
-                        rolesList={rolesList}
+              />
+            ))}
+          </div>
 
-                    />
-                ))}
-            </div>
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default UsersList;
