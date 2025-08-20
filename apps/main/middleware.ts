833e49566e8687adbed9c832e57dd2da2e12f8be
Sudhakar bansal <sidhu1171bansal@gmail.com>
2025-08-20 15:12:16 +0530

    minor fix

--- apps/main/middleware.ts
+++ apps/main/middleware.ts
@@ -29,11 +29,11 @@ export async function middleware(req: NextRequest) {
 
 
   if (authed && (PUBLIC.includes(pathname) || pathname === "/")) {
-    return NextResponse.redirect(new URL("/platform", req.url));
+    return NextResponse.redirect(new URL('/platform', process.env.NEXT_PUBLIC_MAIN_URL!));
   }
 
   if (!authed && !PUBLIC.includes(pathname)) {
-    const loginUrl = new URL("/login", req.url);
+    const loginUrl = new URL("/login", process.env.NEXT_PUBLIC_MAIN_URL!);
 
     const res = NextResponse.redirect(loginUrl);
     res.cookies.delete("next-auth.session-token");
