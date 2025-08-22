c7da4497750b242883ec9190be00802fbfbfd174
Sudhakar bansal <sidhu1171bansal@gmail.com>
2025-08-22 12:17:26 +0530

    test

--- apps/main/app/(root)/(settings)/organization/page.tsx
+++ apps/main/app/(root)/(settings)/organization/page.tsx
@@ -30,7 +30,7 @@ export default async function Page({ searchParams }: { searchParams: any }) {
       description="Manage Organizations"
       config={pageLayoutPresets.dashboard}
       buttons={actions}
-      fallback={<OrganizationPageSkeleton />}
+      // fallback={<OrganizationPageSkeleton />}
     >
       <OrganizationFilters />
       <OrganizationDataListing searchParams={resolvedSearchParams} />
