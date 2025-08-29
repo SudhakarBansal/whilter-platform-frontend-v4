3114003de1463eae96c9d7f7a0a808a2f68e913e
Sudhakar bansal <sidhu1171bansal@gmail.com>
2025-08-29 11:22:47 +0530

    minor typo fix

--- apps/media-tools/components/tool-media-panels/LipSyncMediaPanel.tsx
+++ apps/media-tools/components/tool-media-panels/LipSyncMediaPanel.tsx
@@ -1,18 +1,18 @@
 import FileUploadWrapper from "@/components/file-upload/FileUploadWrapper";
 
 export function LipSyncMediaPanel() {
-    return (
-        <FileUploadWrapper
-            type="video"
-            label="Source Audio"
-            heading="Upload Source Audio"
-            subheading="Add your Files here"
-            footer="Only support .wav, mp3 and Audio files"
-            acceptedFormats={['.mp4']}
-            maxFileSize={10}
-            // onUpload={handleUpload}               // Called after successful upload
-            // onFileSelected={handleFileSelected}   // Called when file is selected
-            // onFileRemoved={handleFileRemoved}     // Called when uploaded file is removed
-        />
-    );
+  return (
+    <FileUploadWrapper
+      type="video"
+      label="Source Video"
+      heading="Upload Source Video"
+      subheading="Add your Files here"
+      footer="Only support .mp4 and Video files"
+      acceptedFormats={[".mp4"]}
+      maxFileSize={10}
+      // onUpload={handleUpload}               // Called after successful upload
+      // onFileSelected={handleFileSelected}   // Called when file is selected
+      // onFileRemoved={handleFileRemoved}     // Called when uploaded file is removed
+    />
+  );
 }
