6a9474fe319bc74a93112ae2887c4c6845d64e2a
Sudhakar bansal <sidhu1171bansal@gmail.com>
2025-09-10 16:08:20 +0530

    add virtua tryon route

--- apps/media-tools/data/menuItems.data.ts
+++ apps/media-tools/data/menuItems.data.ts
@@ -1,42 +1,47 @@
 export const menuItems = [
-    {
-        icon: '/icons/Home.svg',
-        label: 'Home',
-        href: '/'
-    },
-    {
-        icon: '/icons/lip-sync.svg',
-        label: 'Lip Sync',
-        href: '/lip-sync'
-    },
-    {
-        icon: '/icons/video-replace.svg',
-        label: 'Video Replace',
-        href: '/video-image-replacement'
-    },
-    {
-        icon: '/icons/voice-cloning.svg',
-        label: 'Voice Cloning',
-        href: '/voice-cloning'
-    },
-    {
-        icon: '/icons/text-to-speech.svg',
-        label: 'Text to Speech',
-        href: '/text-to-speech'
-    },
-    {
-        icon: '/icons/super-resolution.svg',
-        label: 'Super Resolution',
-        href: '/super-resolution'
-    },
-    {
-        icon: '/icons/image-scaler.svg',
-        label: 'Image Scaler',
-        href: '/image-scaler'
-    },
-    {
-        icon: '/icons/avatar-video.svg',
-        label: 'Avatar Video',
-        href: '/avatar-video'
-    }
+  {
+    icon: "/icons/Home.svg",
+    label: "Home",
+    href: "/",
+  },
+  {
+    icon: "/icons/lip-sync.svg",
+    label: "Lip Sync",
+    href: "/lip-sync",
+  },
+  {
+    icon: "/icons/video-replace.svg",
+    label: "Video Replace",
+    href: "/video-image-replacement",
+  },
+  {
+    icon: "/icons/voice-cloning.svg",
+    label: "Voice Cloning",
+    href: "/voice-cloning",
+  },
+  {
+    icon: "/icons/text-to-speech.svg",
+    label: "Text to Speech",
+    href: "/text-to-speech",
+  },
+  {
+    icon: "/icons/super-resolution.svg",
+    label: "Super Resolution",
+    href: "/super-resolution",
+  },
+  {
+    icon: "/icons/image-scaler.svg",
+    label: "Image Scaler",
+    href: "/image-scaler",
+  },
+  // {
+  //     icon: '/icons/avatar-video.svg',
+  //     label: 'Avatar Video',
+  //     href: '/avatar-video'
+  // }
+  {
+    icon: "/icons/virtual-tryon.svg",
+    label: "Virtual Tryon",
+    href: "/virtual-tryon",
+  },
 ];
